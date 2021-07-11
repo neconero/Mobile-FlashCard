d91a0452ef416bf09eb0ef2c4583ace9e89b2797
Uchechukwu Iroegbu <neroneco@yahoo.com>
2021-07-10 23:55:57 -0400

    initial deck information and component

---
+++ components/DecksView.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react'
+import { View, Text } from 'react-native'
+
+export class DecksView extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Decks</Text>
+            </View>
+        )
+    }
+}
+
+export default DecksView
---
+++ components/NewDeck.js
@@ -0,0 +1,12 @@
+import React, { Component} from 'react'
+import {View, Text} from 'react-native'
+
+export class NewDeck extends Component {
+    render() {
+        return (
+            <View>
+                <Text>New Deck</Text>
+            </View>
+        )
+    }
+}
